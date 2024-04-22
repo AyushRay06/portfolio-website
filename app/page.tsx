562491bc8d1f2f7df4f996920438b2318c71c83b
AyushRay06 <rayayush47@gmail.com>
2024-04-22 18:52:20 +0530

    comp

--- app/page.tsx
+++ app/page.tsx
@@ -29,7 +29,6 @@ export default function page() {
             <BackgroundBeamsDemo />
           </div>
           <CardHoverEffectDemo />
-
           <MacbookScrollDemo />
         </div>
       </div>
