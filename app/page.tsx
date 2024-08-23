46d40444e9c63fc9bc9c2ae4658ad57ef672a0e7
AyushRay06 <rayayush47@gmail.com>
2024-08-23 12:28:21 +0530

    project-deployment-fix

--- app/page.tsx
+++ app/page.tsx
@@ -30,26 +30,42 @@ export default function page() {
           <div className=" pt-32 mb-10">
             <RetroGridDemo />
           </div>
-          {/* <WavyBackgroundDemo /> */}
-          {/* <CardHoverEffectDemo /> */}
-          {/* <HeroScrollDemo /> */}
-          <BlurInDemo />
-          <div className="grid grid-cols-1 gap-3 sm:grid-cols-2 max-w-[800px] mx-auto">
-            {DATA.projects.map((project, id) => (
-              <BlurFade>
-                <ProjectCard
-                  href={project.href}
-                  key={project.title}
-                  title={project.title}
-                  description={project.description}
-                  dates={project.dates}
-                  tags={project.technologies}
-                  image={project.image}
-                  video={project.video}
-                  links={project.links}
-                />
-              </BlurFade>
-            ))}
+
+          <div className="space-y-12 w-full py-12 max-w-4xl mx-auto">
+            <BlurFade>
+              <div className="flex flex-col items-center justify-center space-y-4 text-center">
+                <div className="space-y-2">
+                  <div className="inline-block rounded-lg bg-foreground text-background px-3 py-1 text-sm">
+                    My Projects
+                  </div>
+                  <h2 className="text-3xl font-bold tracking-tighter sm:text-5xl">
+                    Check out my latest work
+                  </h2>
+                  <p className="text-muted-foreground md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed">
+                    I&apos;ve worked on a variety of projects, from simple
+                    websites to complex web applications. Here are a few of my
+                    favorites.
+                  </p>
+                </div>
+              </div>
+            </BlurFade>
+            <div className="grid grid-cols-1 gap-3 sm:grid-cols-2 max-w-[800px] mx-auto">
+              {DATA.projects.map((project, id) => (
+                <BlurFade key={project.title}>
+                  <ProjectCard
+                    href={project.href}
+                    key={project.title}
+                    title={project.title}
+                    description={project.description}
+                    dates={project.dates}
+                    tags={project.technologies}
+                    image={project.image}
+                    video={project.video}
+                    links={project.links}
+                  />
+                </BlurFade>
+              ))}
+            </div>
           </div>
 
           <div className="space-y-12 max-w-2xl mx-auto ml-50 py-12">
