d07d9268697e21e0eff93426ae6e4f778aed329c
AyushRay06 <rayayush47@gmail.com>
2024-08-26 00:08:07 +0530

    Confetti-button

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,14 +1,16 @@
+"use client"
 import BlurFade from "@/components/magicui/blur-fade"
 import Link from "next/link"
+import { ConfettiFireworks } from "./confetti"
 
 export const Footer = () => {
   return (
-    <div className="grid items-center justify-center gap-4 px-4 text-center md:px-6 w-full py-12">
+    <div className="grid items-center justify-center gap-4 px-4 text-center md:px-6 w-full py-12 mt-10">
       <BlurFade>
         <div className="space-y-3">
-          <div className="inline-block rounded-lg bg-foreground text-background px-3 py-1 text-sm">
+          {/* <div className="inline-block rounded-lg bg-foreground text-background px-3 py-1 text-sm">
             Contact
-          </div>
+          </div> */}
           <h2 className="text-3xl font-bold tracking-tighter sm:text-5xl">
             Get in Touch
           </h2>
@@ -20,8 +22,11 @@ export const Footer = () => {
             >
               with a direct question on twitter
             </Link>{" "}
-            and I&apos;ll respond whenever I can.
+            and I&apos;ll respond whenever I can. Thanks for visiting Give me a
           </p>
+        </div>
+        <div className="my-5">
+          <ConfettiFireworks />
         </div>
       </BlurFade>
     </div>
