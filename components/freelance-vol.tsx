4feaaf312d20c5e8cc21fbd15b82b290b1865879
AyushRay06 <rayayush47@gmail.com>
2024-09-20 22:20:24 +0530

    photos

--- components/freelance-vol.tsx
+++ components/freelance-vol.tsx
@@ -31,10 +31,17 @@ export function WorkTimeline() {
       ),
     },
     {
-      title: "Early 2023",
+      title: "Hackathon Organizer",
       content: (
         <div>
-          {/* description */}
+          <span className="prose dark:prose-invert text-sm md:text-base text-muted-foreground">
+            Organized a successful hackathon with 100+ participants, focusing on
+            trending problem statements such as AI-powered chatbots, real-time
+            collaboration tools, blockchain-based voting systems, and
+            sustainability-focused tech solutions. The event fostered innovation
+            and teamwork, providing participants with hands-on experience in
+            solving real-world challenges.
+          </span>
           <div className="grid grid-cols-2 gap-4">
             <Image
               src=""
@@ -44,7 +51,7 @@ export function WorkTimeline() {
               className="rounded-lg object-cover h-20 md:h-44 lg:h-60 w-full shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset]"
             />
             <Image
-              src=""
+              src="/work2/hack.jpeg"
               alt="feature template"
               width={500}
               height={500}
@@ -55,12 +62,15 @@ export function WorkTimeline() {
       ),
     },
     {
-      title: "Changelog",
+      title: "Technical Talks",
       content: (
         <div>
-          <p className="text-neutral-800 dark:text-neutral-200 text-xs md:text-sm font-normal mb-4">
-            Deployed 5 new components on Aceternity today
-          </p>
+          <span className="prose dark:prose-invert text-sm md:text-base text-muted-foreground">
+            Delivered 10+ talks as a Technical Speaker at GDSC events and
+            college tech fests, covering Git, GitHub, open source, Docker, and
+            software development. Focused on simplifying concepts and sharing
+            best practices.
+          </span>
           <div className="mb-8"></div>
           <div className="grid grid-cols-2 gap-4">
             <Image
