5971112bde2165eddb465aa7c0185e51f37dbf18
AyushRay06 <rayayush47@gmail.com>
2024-10-15 02:21:05 +0530

    clean-up

--- components/sparkel-text.tsx
+++
@@ -1,9 +0,0 @@
-import SparklesText from "@/components/magicui/sparkles-text"
-
-export async function SparklesTextDemo() {
-  return (
-    <div className="flex items-center justify-center pb-6">
-      <SparklesText text="Projects" />
-    </div>
-  )
-}
